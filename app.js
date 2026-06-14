53ec31e236103bf8a555c775cb1d46338c1a0f96
rootwav <98337319+rootwav@users.noreply.github.com>
2026-06-14 17:58:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// TEST VULNERABILITY FOR CODEQL - REMOVE AFTER TESTING
+function unsafeRedirect() {
+    const params = new URLSearchParams(window.location.search);
+    const next = params.get("next");
+
+    if (next) {
+        window.location.href = next;
+    }
+}
+
+unsafeRedirect();
 function changeText(){
 
 document.getElementById("message").innerHTML =
